9d7f7d72eb8c861575d8ef6eb833edfc5c278200
Vipul-Pandey-22 <vipulpandeyofficial@gmail.com>
2022-11-09 11:55:48 +0530

    play with SELECT in SQL

---
+++ operations_with_select.sql
@@ -0,0 +1,44 @@
+-- OPERATIONS WITH SELECT COMMAND
+
+-- list of all tables
+show tables;
+
+-- CREATE A TABLE
+create table if not exists employee
+(
+	id int,
+    `name` varchar(50),
+    age int,
+    hiring_date date,
+    salary int,
+    city varchar(50)
+);
+
+insert into employee values(1, 'ABC', 22, '2022-12-07', 25000, 'blr');
+insert into employee values(2, 'XYZ', 23, '2022-12-15', 28000, 'hyd');
+insert into employee values(3, 'CDE', 22, '2022-07-01', 13000, 'wfh');
+insert into employee values(4, 'MNO', 24, '2022-04-12', 15000, 'wfh');
+
+-- Display all the columns
+select * from employee;
+
+-- How to count total records
+select count(*) as total_records from employee;
+
+-- Display specific columns
+select `name`, 
+		salary 
+from employee;
+
+-- How many unique hiring_dates in the employee table when employee joined it
+select distinct(hiring_date) as unique_hiring_dates from employee;
+
+-- How many unique age values in the employee table
+select count(distinct(age)) from employee;
+
+-- Increment salary of each employee by 20% and display final result with new result
+select id, 
+	   `name`, 
+       salary as old_salary, 
+       round(salary + (salary * 20/100), 2) as incremented_salary 
+from employee;
