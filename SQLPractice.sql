212e0cec2cd593b63274653b44877ef204b9e95c
Vipul-Pandey-22 <vipulpandeyofficial@gmail.com>
2022-11-10 17:53:08 +0530

    Update SQLPractice.sql

--- SQLPractice.sql
+++ SQLPractice.sql
@@ -203,5 +203,8 @@ from patients;
 --- Show all columns for patients who have one of the following patient_ids:
 --- 1,45,534,879,1000
 
+select * from patients where patient_id in (1, 45, 534, 879, 1000);
+
+
 
 
