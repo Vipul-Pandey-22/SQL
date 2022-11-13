eefb189a614dfddbdc0a09879c3904543f11c99c
Vipul-Pandey-22 <vipulpandeyofficial@gmail.com>
2022-11-13 11:55:30 +0530

    Update groupby.sql

--- groupby.sql
+++ groupby.sql
@@ -27,3 +27,42 @@ SELECT * FROM orders_data;
 --- Use of HAVING clause
 # write a query to find the country where only 1 order was placed
 SELECT country FROM orders_data GROUP BY country Having count(*) = 1;
+
+# WHERE clause and GROUP BY clasue --> what should by proper sequence to be followed
+# Answer --> WHERE clasue and then GROUP BY
+
+# How to use GROUP_CONCAT
+-- Query - Write a query to print distinct states present in the dataset for each country?
+    -- This will show all the states
+SELECT 
+    country, 
+    GROUP_CONCAT(`state`) as states_in_country 
+FROM 
+    orders_data 
+GROUP BY country;
+    -- This will show only DISTINCT state
+SELECT 
+    country, 
+    GROUP_CONCAT(DISTINCT `state`) as states_in_country 
+FROM 
+    orders_data 
+GROUP BY country;
+
+    -- Arrange the data
+SELECT 
+    country, 
+    GROUP_CONCAT(DISTINCT `state` ORDER BY `state` DESC) as states_in_country 
+FROM 
+    orders_data 
+GROUP BY country ;
+
+    -- Use different seperator
+SELECT 
+    country, 
+    GROUP_CONCAT(DISTINCT `state` ORDER BY `state` DESC separator ' - ') as states_in_country 
+FROM 
+    orders_data 
+GROUP BY country ;
+
+
+
